15a24e44e1c5cc75d0f02c3b7a7c2423ab9849db
Victor Skits <vityok.nice@gmail.com>
2024-06-19 12:40:05 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const width = wall.clientWidth;
+const height = wall.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const spiderTop = (height - spiderHeight) / 2;
+const spiderLeft = (width - spiderWidth) / 2;
+spider.style.top = `${spiderTop}px`;
+spider.style.left = `${spiderLeft}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
